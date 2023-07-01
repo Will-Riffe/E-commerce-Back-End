06e1737ee1bbcbb2deaf4dd775ad15837629052b
Will Riffe <w_riffe@protonmail.com>
2023-06-30 20:37:55 -0400

    added comments to ProductTag model

--- Main/models/ProductTag.js
+++ Main/models/ProductTag.js
@@ -6,12 +6,14 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    // Adds the id (#) of the productTag to this model
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    // Adds the id(#) of the associated product
     product_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -19,13 +21,15 @@ ProductTag.init(
         key: 'id',
       },
     },
+    // Adds the associated tag id(#) to the model
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'tag',
         key: 'id',
       },
-    },  },
+    },  
+  },
   {
     sequelize,
     timestamps: false,
